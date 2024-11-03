73d2dc31e192f1717416af0e6700daad4b007a70
“Balajee <balajee.84068@gmail.com>
2024-11-03 17:22:04 +0530

    added the changes

--- router/user.js
+++ router/user.js
@@ -38,6 +38,21 @@ catch(err){
 }
 })
 
+router.get("/checkstatus/:id",async(req,res,next)=>{
+  try{
+  const {id} = req.params; 
+  const user = await User.findById(id);
+  if(!user){
+    return res.status(404).json({message:"Send to splash screen"});
+  }
+  const playingstatus = user.playingstatus;
+  return res.status(200).json({playingstatus}); 
+}
+catch(err){
+  throw err;
+}
+})
+
 router.patch("/changestatus/:id",async(req,res,next)=>{
   try{
   const {id} = req.params; 
