b20a0b1d8a367597e4709b52d4d101dfe2745feb
balajee <balajee.84068@gmail.com>
2024-11-13 08:47:10 +0000

    some changes

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(async (err, req, res, next) => {
   return res.status(err.status).json({ message: err.message });
 });
 
-app.listen(8000, () => console.log("Server started"))
+app.listen(3000, () => console.log("Server started"))
 
 
