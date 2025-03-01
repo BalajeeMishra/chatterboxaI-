332d43dcd8099f68a98dd80b247167309f3ade39
KHUSHVANT MADODIYA <khushvantmadodiya@gmail.com>
2025-03-01 20:16:35 +0530

    delete unused A.java

--- A.java
+++
@@ -1,8 +0,0 @@
-signingConfigs {
-    release {
-        storeFile file("my-release-key.jks")
-        storePassword "your-password"
-        keyAlias "my-key-alias"
-        keyPassword "your-key-password"
-    }
-}
