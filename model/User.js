b7fed2ca2ecec1f4261bd24e1f952a84c828a2f0
Balajee Mishra <balajee.84068@gmail.com>
2025-05-25 23:36:40 +0530

    remove unique and required constraints from mobileNo field in user schema

--- model/User.js
+++ model/User.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     mobileNo:{
         type:String, 
-        unique:true,
-        required: true
     },
     name: {
         type: String,
